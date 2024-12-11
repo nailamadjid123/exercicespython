6b2b124d86ab1bccf571190b71a260741e087d58
NAILA ZEMMACHE <155105829+nailamadjid123@users.noreply.github.com>
2024-12-11 12:23:37 +0100

    commit

---
+++ exo1.py
@@ -0,0 +1,7 @@
+name=str(input("Please enter your name:"))
+if name=="VIP":
+    print("Enjoy the show for free!")
+else :
+    nbrticket=int(input("How many tickets would you like to buy?"))
+    print("The total cost is",nbrticket*15.50)
+    print("Enjoy your tickets!")
