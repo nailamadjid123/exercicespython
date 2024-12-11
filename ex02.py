c736e69da5fd0bbd6711a5bf8146ec3e435c5633
NAILA ZEMMACHE <155105829+nailamadjid123@users.noreply.github.com>
2024-12-11 12:36:48 +0100

    commit

---
+++ ex02.py
@@ -0,0 +1,10 @@
+temper=int(input("Please type in the temperature:"))
+if temper < 0:
+    print("It's freezing!")
+if temper< 10:
+      print("It's cold!")
+if temper<20:
+     print("It's cool!")
+     print("Stay safe!")
+else:
+    print("Stay safe!")
