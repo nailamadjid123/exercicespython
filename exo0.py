b4786f7fa5ff1e488cd2b9bb250b7849754d220b
NAILA ZEMMACHE <155105829+nailamadjid123@users.noreply.github.com>
2024-12-11 12:08:42 +0100

    commit

--- exo0.py
+++ exo0.py
@@ -1,1 +1,12 @@
-test
+question1=int(input("how many people need a ride ?"))
+question2=int(input("How many people fit in one taxi? "))
+z=question1 // question2
+if question1 % question2 != 0:
+    z=z+1
+print("Number of taxis needed:",z)
+
+
+
+
+
+
